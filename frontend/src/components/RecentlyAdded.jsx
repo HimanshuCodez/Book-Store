5f3317de96cb36381acd6b4e12a8fab0a9e3b515
Himanshu gaur <himanshugaur055@gmail.com>
2024-11-04 12:45:34 +0530

    loader

---
+++ frontend/src/components/RecentlyAdded.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+import axios from "axios";
+import Cards from "./Cards";
+const  RecentlyAdded = ()=> {
+  
+  const [Data, setData] = useState();
+  useEffect(() => {
+    // Fetch data from API
+    const fetch = async () => {
+      const response = await axios.get(
+        "http://localhost:4000/api/v1/get-recent-books"
+      );
+     setData(response.data.data);
+    };
+    fetch();
+  }, []);
+
+  
+  var settings = {
+    dots: true, // Show navigation dots
+    infinite: true, // Infinite loop sliding
+    speed: 500, // Slide transition speed (ms)
+    slidesToShow: 4, // Number of cards to show at once
+    slidesToScroll: 1, // Number of cards to scroll at once
+    autoplay: true, // Enable automatic sliding
+    autoplaySpeed: 1500,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div>
+          <h1 className="font-semibold text-xl pb-2">Recently Added</h1>
+        </div>
+
+        <div>
+          <Slider {...settings}>
+            {Data && Data.map((item,i) => (
+              <div key={i}>
+              
+              <Cards  data={item} key={item.id} />
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </>
+  );
+}
+export default RecentlyAdded;
